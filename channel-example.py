50f39ae2aa41c13bbf06e040f87abd35d36d2589
vins31 <vincentweb31@gmail.com>
2013-02-24 19:49:14 +0100

    captor --> sensor

--- channel-example.py
+++ channel-example.py
@@ -3,7 +3,7 @@
 from time import sleep
 import socket
 from channel import Channel
-from protos import Captor
+from protos import Sensor
 
 def print_packet(name, args): 
     print("[%s]" %name) 
@@ -15,14 +15,14 @@ if __name__=="__main__":
     socket = socket.socket()
     socket.connect(("localhost", 1305))
    
-    # Création du channel, en utilisant la norme « Captor », sur la socket
+    # Création du channel, en utilisant la norme « sensor », sur la socket
     # ouverte précédemment, et en appelant la fontion print_packet à la
     # réception d’un packet.
-    captor = Channel(Captor, socket, print_packet)
+    sensor = Channel(Sensor, socket, print_packet)
     
     while True:
         ## On envoit de temps en temps un packet.
-        captor.getValue(3)
+        sensor.getValue(3)
         sleep(1)
-        captor.getId()
+        sensor.getId()
         sleep(1)
