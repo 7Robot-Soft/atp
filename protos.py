4fa5ccde7dd6ff7bc26993fa253241ee8c990f04
Élie Bouttier <elie.bouttier@free.fr>
2013-03-02 08:15:42 +0100

    Fix & add genAll option (mark all packet as both)

--- protos.py
+++ protos.py
@@ -1,4 +1,3 @@
-import semantic
 import inspect
 from collections import OrderedDict
 
@@ -17,35 +16,48 @@ class Packet:
         self.attrs = attrs
 
 
-def load():
+def load(genAll = False):
+
+    import semantic
+
     protos = OrderedDict()
 
     for name, proto in inspect.getmembers(semantic,
             lambda x: inspect.isclass(x) and issubclass(x, semantic.Proto)):
         if name != "Proto" and name != "Common":
-            protos[name] = load_proto(proto)
+            protos[name] = load_proto(proto, genAll)
 
     return protos
 
-def load_proto(proto):
+def load_proto(proto, genAll = False):
+
+    import semantic
+
     p = OrderedDict()
+
+    p['id'] = proto.type
+    p['packets'] = OrderedDict()
 
     for name, packet in inspect.getmembers(semantic.Common,
             lambda x: isinstance(x, semantic.Packet)):
-        p[name] = load_packet(packet)
+        p['packets'][name] = load_packet(packet, genAll)
 
     for name, packet in inspect.getmembers(proto,
             lambda x: isinstance(x, semantic.Packet)):
-        p[name] = load_packet(packet)
+        p['packets'][name] = load_packet(packet, genAll)
 
     return p
 
-def load_packet(packet):
+def load_packet(packet, genAll = False):
     p = OrderedDict()
 
     p['id'] = packet.id
-    p['direction'] = packet.direction
+    if genAll:
+        p['direction'] = 'both'
+    else:
+        p['direction'] = packet.direction
+    p['args'] = OrderedDict()
     for arg, type in packet.attrs:
-        p[arg] = type
+        p['args'][arg] = type
 
     return p
