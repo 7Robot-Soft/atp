f0d8dd02ab955ecf621d7c79257aa98aad71dc06
7Robot <robot@list.bde.enseeiht.fr>
2013-04-21 01:04:52 +0200

    Add Ax and Funny messages

--- semantic.py
+++ semantic.py
@@ -86,3 +86,76 @@ class Sensor(Proto):
             ("id", "B"),
             ("threshold", "f")
         ])
+
+class Ax(Proto):
+    type = 3
+
+    GetPos = Packet(1, "arm", [
+            ("idpic", "B")
+        ])
+    Pos = Packet(2, "pic", [
+            ("id", "B"),
+            ("value", "f")
+        ])
+
+    BougiesHitTop = Packet(3, "arm", [            
+        ])
+    BougiesHitTopConfirm = Packet(4, "pic", [            
+        ])
+
+    BougiesHitBot = Packet(5, "arm", [            
+        ])
+    BougiesHitBotConfirm = Packet(6, "pic", [            
+        ])
+
+    BougiesOff = Packet(7, "arm", [            
+        ])
+    BougiesOffConfirm = Packet(8, "pic", [            
+        ])
+
+    BougiesOn = Packet(9, "arm", [            
+        ])
+    BougiesOnConfirm = Packet(10, "pic", [            
+        ])
+
+    VerresEnd = Packet(11, "arm", [            
+        ])
+    VerresEndConfirm = Packet(12, "pic", [            
+        ])
+
+    VerresCatch = Packet(13, "arm", [            
+        ])
+    VerresCatchConfirm = Packet(14, "pic", [            
+        ])
+
+    VerresOff = Packet(15, "arm", [            
+        ])
+    VerresOffConfirm = Packet(16, "pic", [            
+        ])
+
+    VerresOn = Packet(17, "arm", [            
+        ])
+    VerresOnConfirm = Packet(18, "pic", [            
+        ])
+
+class Funny(Proto):
+    type = 4
+
+    FunnyAction = Packet(1, "arm", [
+        ])
+    EndFunnyAction = Packet(2, "pic", [
+            ("id", "B")
+        ])
+    setThreshold = Packet(3, "arm", [
+            ("id", "B"),
+            ("threshold", "f")
+        ])
+
+# Rappel des types struct.pack usuelles :
+# B  unsigned char
+# H  unsigned short
+# I  unsigned int 
+# b  signed char
+# h  signed short
+# i  signed int 
+# f  float
