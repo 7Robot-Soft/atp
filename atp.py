ea7b47b1f0c28c5fd505f4da9b5642601178b87f
Élie Bouttier <elie.bouttier@free.fr>
2013-04-21 22:28:31 +0200

    Pour avoir une meilleur note avec pylint …

--- atp.py
+++ atp.py
@@ -1,3 +1,8 @@
+"""
+Module ATP
+Encode et decode les messages ATP à l’aide du module « struct »
+"""
+
 import binascii
 import struct
 import sys
@@ -5,10 +10,13 @@ import time
 from threading import Thread
 from logging import getLogger
 
-formats = { 1 : '<B', 2 : '<H', 4 : '<I', 17 : '<b', 18 : '<h', 20 : '<i', 36 : '<f' }
+FORMATS = { 1 : '<B', 2 : '<H', 4 : '<I', 17 : '<b', \
+        18 : '<h', 20 : '<i', 36 : '<f' }
 
 def async(fcnt):
-
+    """
+    Décorateur pour lancer dans un nouveau thread une fonction.
+    """
     def run(*k, **kw):
         t = Thread(target=fcnt, args=k, kwargs=kw)
         t.start()
@@ -18,97 +26,107 @@ def async(fcnt):
 
 def encode(stream, id, args):
     logger = getLogger("atp.encode")
-    buffer = bytearray()
-    buffer += struct.pack('B', 129)
-    buffer += struct.pack('B', int(id))
-    for value, type in args:
+    buf = bytearray()
+    buf += struct.pack('B', 129)
+    buf += struct.pack('B', int(id))
+    for value, form in args:
         code = None
-        for c, f in formats.items():
-            if type == f[1]:
+        for c, f in FORMATS.items():
+            if form == f[1]:
                 code = c
                 break
         if code == None:
-            logger.warning("unknow type (%c), assuming unsigned char" %type)
-            buffer += struct.pack('B', 128)
+            logger.warning("unknow type (%c), assuming unsigned char" %form)
+            buf += struct.pack('B', 128)
             return
-        if type == 'f':
+        if form == 'f':
             value = float(value)
         else:
             value = int(value)
-        buffer += struct.pack('B', code)
-        buffer += struct.pack('<'+type, value)
-    buffer += struct.pack('B', 128)
-    stream.write(buffer)
+        buf += struct.pack('B', code)
+        buf += struct.pack('<'+form, value)
+    buf += struct.pack('B', 128)
+    stream.write(buf)
     stream.flush()
 
 @async
 def decode(stream, callback, follow = False):
+    """
+    Décode les messages ATP.
+    Si follow = False, appelle callback avec id = -1 lorsque la fin du flux est
+    atteinte.
+    Si follow = True, reessaye de lire le flux après une temporisation lorsque
+    la fin est atteinte.
+    """
 
     logger = getLogger("atp.decode")
     data = b''
     expected = 'begin'
     length = 1
-    format = ''
-    id = 0
-    errors = 0
+    form = ''
+    paquet_id = 0
+    #errors = 0
     args = []
 
     while True:
-        c = stream.read(1)
-        if not len(c):
+        pending = stream.read(1)
+        if not len(pending):
             if follow:
                 time.sleep(0.1)
                 continue
             else:
                 callback(-1, [])
                 sys.exit()
-        data += c
+        data += pending
         while len(data) >= length:
             if expected == 'begin':
-                c = data[0]
+                pending = data[0]
                 data = data[1:]
-                if c == 129: # Début de trame
+                if pending == 129: # Début de trame
                     expected = 'id'
                 else:
-                    logger.warning('expected beginning flag (%d)' %c)
-                    errors += 1
+                    logger.warning('expected beginning flag (%d)' %pending)
+                    #errors += 1
             elif expected == 'id':
-                c = data[0]
+                pending = data[0]
                 data = data[1:]
-                id = c
+                paquet_id = pending
                 args = []
                 expected = 'type'
             elif expected == 'type':
-                c = data[0]
+                pending = data[0]
                 data = data[1:]
-                if c == 128: # Fin de trame
-                    callback(id, args)
+                if pending == 128: # Fin de trame
+                    callback(paquet_id, args)
                     expected = 'begin'
-                elif c == 132: # timestamp
-                    format = formats[4]
-                    length = c & 0b1111
+                elif pending == 132: # timestamp
+                    form = FORMATS[4]
+                    length = pending & 0b1111
                     value = b''
                     expected = 'data'
-                elif c == 148: # microseconds
-                    format = formats[4]
-                    length = c & 0b1111
+                elif pending == 148: # microseconds
+                    form = FORMATS[4]
+                    length = pending & 0b1111
                     value = b''
                     expected = 'data'
                 else:
                     try:
-                        format = formats[c]
+                        form = FORMATS[pending]
                     except KeyError:
-                        logger.warning('unknow format (%d), waiting next flag' %c)
-                        errors += 1
+                        logger.warning('unknow format (%d)" \
+                                ", waiting next flag' %pending)
+                        #errors += 1
                         expected = 'begin'
-                    length = c & 0b1111
+                    length = pending & 0b1111
                     value = b''
                     expected = 'data'
             elif expected == 'data':
                 try:
-                    value = struct.unpack(format, data[0:length])
+                    value = struct.unpack(form, data[0:length])
                 except struct.error:
-                    logger.warning("struct.unpack exception, ignoring argument %s of format %s" % (binascii.hexlify(data[0:length]), format))
+                    logger.warning("struct.unpack exception" \
+                            ", ignoring argument %s of format %s" \
+                            %(binascii.hexlify(data[0:length]), form))
                 else:
                     args.append(value[0])
                 finally:
