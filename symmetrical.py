d0bc132fdbf0004cf469d5e9991cbf2b20ca406d
vins31 <vincentweb31@gmail.com>
2013-05-09 02:58:30 +0200

    non mais symmétrie il bouffe des strings, il crache des strings

--- symmetrical.py
+++ symmetrical.py
@@ -1,6 +1,18 @@
 def symmetrical(proto, name, args):
 
     if proto == "asserv":
-
         if name == "pos" or name == "setPos":
-            args["x"] = - int(args["x"])
+            args["x"] = str(-int(args["x"]))
+
+        elif name == "angle" or name == "setAngle":
+            args["theta"] = str(-int(args["theta"]))
+
+        elif name == "omega" or name == "setOmega":
+            args["omega"] = str(-int(args["omega"]))
+
+        elif name == "getSICKValue" or name == "SICKValue" or name == "setThreshold":
+            # convention : 0 avantG, 1 avanT, 2 arrière
+            if args["id"] == "0":
+                args["id"] = "1"
+            elif args["id"] == "1":
+                args["id"] = "0"
